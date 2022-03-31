ccc3f6e602ff4bb254257a1da247c3f4574829ec
xiongwenhui <xiongwh@mail.open.com.cn>
2022-03-31 16:00:09 +0800

    init

---
+++ src/Factory/Base/FileTemplate.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Mili\Esign\Factory\Base;
+
+use Mili\Esign\Factory\Filetemplate\CreateFileByTemplate;
+use Mili\Esign\Factory\Filetemplate\CreateTemplateByUploadUrl;
+use Mili\Esign\Factory\Filetemplate\GetFileUploadUrl;
+use Mili\Esign\Factory\Filetemplate\UploadFile;
+
+/**
+ * 轩辕API
+ * @author  澄泓
+ * @date  2020/11/23 10:29
+ */
+class FileTemplate
+{
+    /**
+     * 通过模板创建文件
+     * @param $name
+     * @param $templateId
+     * @param $simpleFormFields
+     * @return CreateFileByTemplate
+     */
+    public static function createFileByTemplate($name, $templateId, $simpleFormFields){
+        return new CreateFileByTemplate($name, $templateId, $simpleFormFields);
+    }
+
+    /**
+     * 通过上传方式创建模板
+     * @param $contentMd5
+     * @param $contentType
+     * @param $fileName
+     * @param $convert2Pdf
+     * @return CreateTemplateByUploadUrl
+     */
+    public static function createTemplateByUploadUrl($contentMd5, $contentType, $fileName, $convert2Pdf){
+        return new CreateTemplateByUploadUrl($contentMd5, $contentType, $fileName, $convert2Pdf);
+    }
+
+    /**
+     * 通过上传方式创建文件
+     * @param $contentMd5
+     * @param $contentType
+     * @param $convert2Pdf
+     * @param $fileName
+     * @param $fileSize
+     * @return GetFileUploadUrl
+     */
+    public static function getFileUploadUrl($contentMd5, $contentType, $convert2Pdf, $fileName, $fileSize){
+        return new GetFileUploadUrl($contentMd5, $contentType, $convert2Pdf, $fileName, $fileSize);
+    }
+
+    /**
+     * 上传文件
+     * @param $filePath
+     * @param $contentType
+     * @param $url
+     * @return UploadFile
+     */
+    public static function uploadFile($filePath, $contentType, $url){
+        return new UploadFile($filePath, $contentType, $url);
+    }
+}
---
+++ src/Factory/Signfile/Pdfverify/PdfVerify.php
@@ -0,0 +1,88 @@
+<?php
+namespace Mili\Esign\Factory\Signfile\Pdfverify;
+use Mili\Esign\Factory\Request\EsignRequest;
+use Mili\Esign\Emun\HttpEmun;
+/**
+ * 轩辕API文件验签
+ * @author  澄泓
+ * @date  2020/11/24 14:43
+ */
+class PdfVerify extends EsignRequest implements \JsonSerializable
+{
+    private $fileId;
+    private $flowId;
+
+    /**
+     * PdfVerify constructor.
+     * @param $fileId
+     */
+    public function __construct($fileId)
+    {
+        $this->fileId = $fileId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileId()
+    {
+        return $this->fileId;
+    }
+
+    /**
+     * @param mixed $fileId
+     * @return PdfVerify
+     */
+    public function setFileId($fileId)
+    {
+        $this->fileId = $fileId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFlowId()
+    {
+        return $this->flowId;
+    }
+
+    /**
+     * @param mixed $flowId
+     * @return PdfVerify
+     */
+    public function setFlowId($flowId)
+    {
+        $this->flowId = $flowId;
+        return $this;
+    }
+
+    function build()
+    {
+        $url="/v1/documents/".$this->fileId."/verify?";
+        if($this->flowId!==null){
+            $url=$url."&flowId=".$this->flowId;
+        }
+        $this->setUrl($url);
+        $this->setReqType(HttpEmun::GET);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        $json = array();
+        foreach ($this as $key => $value) {
+            if($value===null||$key=='flowId'||$key=='fileId') {
+                continue;
+            }
+            $json[$key] = $value;
+        }
+        return $json;
+    }
+}
